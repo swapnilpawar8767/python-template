5f35717b278b233980551f0958df739ccc258220
Swap-1-test <swap12321232@gmail.com>
2025-07-29 11:57:55 +0530

    test:update file with version

--- src/main.py
+++ src/main.py
@@ -1,12 +1,15 @@
 import time
 
+
 def main():
     print("🚀 Python Template Project is ready!")
     while True:
-        time.sleep(60)  # Keep the container running
+        time.sleep(60)
+
 
 def add(a, b):
     return a + b
+
 
 if __name__ == "__main__":
     main()
